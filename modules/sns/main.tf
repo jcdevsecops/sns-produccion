704764f6a8dc6d2dce38de7fd83534452731a382
Carlos Jerson Angulo Lopez <carlos.angulo@sofka.local>
2024-03-07 17:55:39 -0500

    FIX path module

--- modules/sns/main.tf
+++ modules/sns/main.tf
@@ -1,3 +1,32 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-first-test-bucket-ca"
+resource "aws_sns_topic" "example" {
+  name = "my-first-sns-topic"  
+}
+
+resource "aws_sns_topic_subscription" "sms_subscription" {
+  topic_arn = aws_sns_topic.example.arn
+  protocol  = "sms"
+  endpoint  = "+573203278909"  
+}
+
+resource "aws_sns_topic_policy" "example_policy" {
+  arn    = aws_sns_topic.example.arn
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "example_policy",
+  "Statement": [
+    {
+      "Sid": "example_statement",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "SNS:Publish",
+        "SNS:Subscribe",
+        "SNS:Receive"
+      ],
+      "Resource": "arn:aws:sns:us-west-2:123456789012:my-first-sns-topic"
+    }
+  ]
+}
+EOF
 }
